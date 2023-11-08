50dfdedf635d785a5de26af20c8ff0705e843041
DisruptionSystemsINC <disruption@gandhithedergrawr.com>
2023-11-08 17:03:54 +0100

    Fluffbot initial commit

--- src/main/EventListeners/SlashCommands/BulkDelete.java
+++ src/main/EventListeners/SlashCommands/BulkDelete.java
@@ -12,24 +12,22 @@ public class BulkDelete extends ListenerAdapter{
         String command = event.getName();
         if (command.equals("bulk-delete")) {
             int amountToDelete = event.getOption("delamount").getAsInt();
-            if (event.getJDA().getRoles().toString().toLowerCase().contains("staff")) {
-                if (amountToDelete > 30) {
-                    event.reply("WARNUNG: Das CHORUS System kann bei Größeren Mengen an Posts das löschen nicht Garantieren! Die Löschung kann mehrere Minuten dauern.").setEphemeral(true).complete();
-                } else {
-                    event.reply("Löschen von " + amountToDelete + " Nachricht(en) wird gestartet... Dies kann etwas Zeit in Anspruch nehmen").setEphemeral(true).complete();
-                }
-                MessageHistory msg = event.getChannel().getHistory();
-                List<Message> Messages = msg.retrievePast(amountToDelete).complete();
-                for (Message message : Messages) {
-                    try {
-                        Thread.sleep(300);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+            if(amountToDelete <= 100) {
+                if (event.getJDA().getRoles().toString().toLowerCase().contains("staff")) {
+                    if (amountToDelete > 30) {
+                        event.reply("WARNUNG: Das CHORUS System kann bei Größeren Mengen an Posts das löschen nicht Garantieren! Die Löschung kann mehrere Minuten dauern.").setEphemeral(true).complete();
+                    } else {
+                        event.reply("Löschen von " + amountToDelete + " Nachricht(en) wird gestartet... Dies kann etwas Zeit in Anspruch nehmen").setEphemeral(true).complete();
                     }
-                    event.getChannel().deleteMessageById(message.getId()).complete();
+                    MessageHistory msg = event.getChannel().getHistory();
+                    List<Message> Messages = msg.retrievePast(amountToDelete).complete();
+                    event.getChannel().asTextChannel().deleteMessages(Messages).complete();
+                } else {
+                    event.reply("Du musst Staff sein um diesen Command zu nutzen.").setEphemeral(true).complete();
                 }
-            } else {
-                event.reply("Du musst Staff sein um diesen Command zu nutzen.").setEphemeral(true).complete();
+            }
+            else {
+                event.reply("FEHLER: Das Chorus System kann aufgrund einer Discord API limitation ***Maximal*** 100 Nachrichten auf einmal löschen. Um mehr zu löschen führe den Befehl nochmal aus.").setEphemeral(true).complete();
             }
         }
     });
