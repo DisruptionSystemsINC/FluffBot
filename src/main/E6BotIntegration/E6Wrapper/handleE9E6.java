1b469b6b9e74ea67a1c65d65c11950f94b62f59f
DisruptionSystemsINC <disruption@gandhithedergrawr.com>
2023-11-10 20:15:23 +0100

    E6Bot integration completed but still missing channels, waiting for Feeback

--- src/main/E6BotIntegration/E6Wrapper/handleE9E6.java
+++ src/main/E6BotIntegration/E6Wrapper/handleE9E6.java
@@ -2,7 +2,32 @@ package main.E6BotIntegration.E6Wrapper;
 
 public class handleE9E6 {
     public static String handleE6(String type, String customType){
-
+        switch(type){
+            case ("gay-anal") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("gay+anal");
+            }
+            case ("gay-threesome") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("gay+threesome");
+            }
+            case("lesbian") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("lesbian");
+            }
+            case ("female-solo") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("female+solo");
+            }
+            case ("male-solo") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("male+solo");
+            }
+            case ("straight") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("straight");
+            }
+            case ("lesbian-threesome") -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET("lesbian+threesome");
+            }
+            default -> {
+                return new main.E6BotIntegration.E6Wrapper.HTTPRequestHandler().e6GET(customType);
+            }
+        }
     }
     public static String handleE9(String type, String customType){
         switch(type){
