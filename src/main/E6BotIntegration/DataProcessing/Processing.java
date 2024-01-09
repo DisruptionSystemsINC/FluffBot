81b297c379331797c648bd49c596d4bb9ab846a9
DisruptionSystemsINC <disruption@gandhithedergrawr.com>
2024-01-09 19:16:05 +0100

    Added artist to Posts

--- src/main/E6BotIntegration/DataProcessing/Processing.java
+++ src/main/E6BotIntegration/DataProcessing/Processing.java
@@ -3,7 +3,6 @@ package main.E6BotIntegration.DataProcessing;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import main.E6BotIntegration.Blacklist.Blacklist;
 import main.E6BotIntegration.E6Wrapper.handleE9E6;
 import main.EventListeners.utility.Logging;
 
@@ -12,28 +11,23 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import static main.E6BotIntegration.Blacklist.Blacklist.blacklist;
 
 public class Processing {
 
     public String ProcessorNSFW(String HTTPContent) throws IOException {
 
-        for (String s : Blacklist.getNSFWBlacklist()) {
+        for (String s : blacklist) {
             for (String a : getGeneralTags(HTTPContent)) {
                 if ((a).toLowerCase().equals(s)) {
                     System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
+                    handleE9E6.handleE9("", "");
                 }
             }
             for (String b : getSpeciesTags(HTTPContent)) {
                 if ((b).toLowerCase().equals(s)) {
                     System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-            for (String c : getArtistTags(HTTPContent)) {
-                if ((c).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
+                    handleE9E6.handleE9("", "");
                 }
             }
         }
@@ -41,42 +35,34 @@ public class Processing {
                 ObjectMapper objectMapper = new ObjectMapper();
                 JsonNode jsonNode = objectMapper.readTree(HTTPContent);
                 String url = jsonNode.path("posts").get(0).path("file").path("url").asText();
-                String artist = jsonNode.path("posts").get(0).path("tags").path("artist").get(0).asText();
                 if (!Objects.equals(url, "null")) {
-                    Logging.printToLog("Sending post: \n" + url + "\n from: \n" + HTTPContent);
-                    return "Artist: " + artist + "\n" + url;
+                    return url;
                 } else {
                     handleE9E6.handleE6("", "");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-                Logging.printToLog("Warning, There has been an error parsing the url json");
+                System.out.println("Warning, There has been an error parsing the url json");
                 return "WARNUNG: Invalider JSON Syntax. Bitte überprüfe den status der e621 Server und Informiere den bot Operator (disruption@gandhithedergrawr.com)";
 
             }
-            return  handleE9E6.handleE6("", "");
+            return "Fehler bei Verarbeitung: Timeout";
         }
 
 
     public String Processor(String HTTPContent) throws IOException {
 
-        for (String s : Blacklist.getSFWBlacklist()) {
+        for (String s : blacklist) {
             for (String a : getGeneralTags(HTTPContent)) {
                 if ((a).toLowerCase().equals(s)) {
                     System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
+                    handleE9E6.handleE9("", "");
                 }
             }
             for (String b : getSpeciesTags(HTTPContent)) {
                 if ((b).toLowerCase().equals(s)) {
                     System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-            for (String c : getArtistTags(HTTPContent)) {
-                if ((c).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
+                    handleE9E6.handleE9("", "");
                 }
             }
         }
@@ -94,10 +80,10 @@ public class Processing {
             }
         } catch (IOException e) {
             Logging.printToLog("Warning: There has been an error parsing field \"url\"");
-            return "Fehler: Generischer Parserfehler";
+            return null;
         }
         Logging.printToLog("Critical Error 001: Invalid data from endpoint. \nData:\n" + HTTPContent);
-        return handleE9E6.handleE9("", "");
+        return "Fehler bei Verarbeitung: Timeout";
     }
 
     public List<String> getGeneralTags(String json) {
@@ -119,27 +105,6 @@ public class Processing {
         }
         return Collections.emptyList();
     }
-
-    public List<String> getArtistTags(String json) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(json);
-
-            JsonNode generalTagsNode = rootNode
-                    .path("posts")
-                    .path(0)
-                    .path("tags")
-                    .path("artist");
-
-            List<String> generalTags = objectMapper.convertValue(generalTagsNode, List.class);
-
-            return generalTags;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return Collections.emptyList();
-    }
-
     public List<String> getSpeciesTags (String json){
 
         try {
---
+++ src/main/EventListeners/utility/DailyPost.java
@@ -0,0 +1,34 @@
+package main.EventListeners.utility;
+
+import main.E6BotIntegration.E6Wrapper.handleE9E6;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.io.IOException;
+
+public class DailyPost extends ListenerAdapter {
+    @Override
+    public void onGuildReady(GuildReadyEvent event) {
+        Thread thread = new Thread( () -> {
+            while (true){
+                try {
+                    TextChannel channel = event.getGuild().getTextChannelsByName("nsfw-bot", true).get(0);
+                    TextChannel sfwchannel = event.getGuild().getTextChannelsByName("fluffymedia", true).get(0);
+                    channel.sendMessage(handleE9E6.handleE6("", "")).complete();
+                    sfwchannel.sendMessage(handleE9E6.handleE9("", "")).complete();
+                    Logging.printToLog("Daily Post has been triggered");
+                    Thread.sleep((long) 8.64E7);
+                } catch (InterruptedException | IOException e) {
+                    try {
+                        Logging.printToLog("Der dailypost wurde interrupted");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+    }
+}
