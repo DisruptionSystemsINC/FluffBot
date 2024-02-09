1e4e8bf572f26b9955096a521e05ee5e4f9293d0
gandhi <disruption@gandhithedergrawr.com>
2024-02-09 12:04:29 +0100

    Code Optimizations to minimize Duplicate code within functions

--- src/main/E6BotIntegration/DataProcessing/Processing.java
+++ src/main/E6BotIntegration/DataProcessing/Processing.java
@@ -1,12 +1,12 @@
 package main.E6BotIntegration.DataProcessing;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import main.E6BotIntegration.Blacklist.Blacklist;
 import main.E6BotIntegration.E6Wrapper.handleE9E6;
 import main.EventListeners.utility.Logging;
-
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -17,90 +17,95 @@ public class Processing {
 
     public String ProcessorNSFW(String HTTPContent) throws IOException {
 
-        for (String s : Blacklist.getNSFWBlacklist()) {
-            for (String a : getGeneralTags(HTTPContent)) {
-                if ((a).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-            for (String b : getSpeciesTags(HTTPContent)) {
-                if ((b).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-            for (String c : getArtistTags(HTTPContent)) {
-                if ((c).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-        }
+        if (containsBlacklistedTag(HTTPContent, false)) {
+            return handleE9E6.handleE6("", "");
+        } else {
             try {
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode jsonNode = objectMapper.readTree(HTTPContent);
-                String url = jsonNode.path("posts").get(0).path("file").path("url").asText();
-                String artist = jsonNode.path("posts").get(0).path("tags").path("artist").get(0).asText();
-                if (!Objects.equals(url, "null")) {
-                    Logging.printToLog("Sending post: \n" + url + "\n from: \n" + HTTPContent);
-                    return "Artist: " + artist + "\n" + url;
-                } else {
-                    handleE9E6.handleE6("", "");
-                }
+                String url = getURL(HTTPContent);
+                String artist = getArtist(HTTPContent);
+                Logging.printToLog("Sending post: \n" + url + "\n from: \n" + HTTPContent);
+                return "Artist: " + artist + "\n" + url;
+
             } catch (IOException e) {
-                e.printStackTrace();
                 Logging.printToLog("Warning, There has been an error parsing the url json");
                 return "WARNUNG: Invalider JSON Syntax. Bitte überprüfe den status der e621 Server und Informiere den bot Operator (disruption@gandhithedergrawr.com)";
 
             }
-            return  handleE9E6.handleE6("", "");
         }
+    }
 
 
     public String Processor(String HTTPContent) throws IOException {
 
-        for (String s : Blacklist.getSFWBlacklist()) {
-            for (String a : getGeneralTags(HTTPContent)) {
-                if ((a).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-            for (String b : getSpeciesTags(HTTPContent)) {
-                if ((b).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-            for (String c : getArtistTags(HTTPContent)) {
-                if ((c).toLowerCase().equals(s)) {
-                    System.out.println("Blacklist Triggered");
-                    return handleE9E6.handleE9("", "");
-                }
-            }
-        }
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(HTTPContent);
-            String url = jsonNode.path("posts").get(0).path("file").path("url").asText();
-            String artist = jsonNode.path("posts").get(0).path("tags").path("artist").get(0).asText();
-            if (!Objects.equals(url, "null") || !Objects.equals(url,null)) {
+        if (containsBlacklistedTag(HTTPContent, true)) {
+            return handleE9E6.handleE9("", "");
+        } else {
+            try {
+                String url = getURL(HTTPContent);
+                String artist = getArtist(HTTPContent);
                 Logging.printToLog("Sending post: \n" + url + "\n from: \n" + HTTPContent);
                 return "Artist: " + artist + "\n" + url;
-            } else {
-                Logging.printToLog("Return URL was null, Retrying");
-                handleE9E6.handleE9("", "");
+
+            } catch (IOException e) {
+                Logging.printToLog("Warning: There has been an error parsing field \"url\"");
+                return "Fehler: Generischer Parserfehler";
             }
-        } catch (IOException e) {
-            Logging.printToLog("Warning: There has been an error parsing field \"url\"");
-            return "Fehler: Generischer Parserfehler";
         }
-        Logging.printToLog("Critical Error 001: Invalid data from endpoint. \nData:\n" + HTTPContent);
-        return handleE9E6.handleE9("", "");
     }
 
-    public List<String> getGeneralTags(String json) {
+    public String getArtist(String content) throws IOException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(content);
+            String artist = jsonNode.path("posts").get(0).path("tags").path("artist").get(0).asText();
+            if (!Objects.equals(artist, "null")) {
+                return artist;
+            }
+            else return "Unknown Artist";
+        } catch (IOException e) {
+            Logging.printToLog("Warning, There has been an error parsing the url json");
+            return "WARNUNG: Invalider JSON Syntax. Bitte überprüfe den status der e621 Server und Informiere den bot Operator (disruption@gandhithedergrawr.com)";
+
+        }
+    }
+
+    public String getURL(String content) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(content);
+        return jsonNode.path("posts").get(0).path("file").path("url").asText();
+    }
+
+    public boolean containsBlacklistedTag(String content, boolean sfw) throws IOException {
+        List<String> blacklist;
+        if (sfw) {
+            blacklist = Blacklist.getSFWBlacklist();
+        } else {
+            blacklist = Blacklist.getNSFWBlacklist();
+        }
+        for (String s : blacklist) {
+            for (String a : getGeneralTags(content)) {
+                if ((a).toLowerCase().equals(s)) {
+                    System.out.println("Blacklist Triggered");
+                    return true;
+                }
+            }
+            for (String b : getSpeciesTags(content)) {
+                if ((b).toLowerCase().equals(s)) {
+                    System.out.println("Blacklist Triggered");
+                    return true;
+                }
+            }
+            for (String c : getArtistTags(content)) {
+                if ((c).toLowerCase().equals(s)) {
+                    System.out.println("Blacklist Triggered");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public List<String> getGeneralTags(String json) throws IOException {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(json);
@@ -111,16 +116,14 @@ public class Processing {
                     .path("tags")
                     .path("general");
 
-            List<String> generalTags = objectMapper.convertValue(generalTagsNode, List.class);
-
-            return generalTags;
+            return objectMapper.convertValue(generalTagsNode, List.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            Logging.printToLog("Warning: Exception thrown in \"Processing.getGeneralTags\"");
         }
         return Collections.emptyList();
     }
 
-    public List<String> getArtistTags(String json) {
+    public List<String> getArtistTags(String json) throws IOException {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(json);
@@ -131,16 +134,14 @@ public class Processing {
                     .path("tags")
                     .path("artist");
 
-            List<String> generalTags = objectMapper.convertValue(generalTagsNode, List.class);
-
-            return generalTags;
+            return objectMapper.convertValue(generalTagsNode, List.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            Logging.printToLog("Warning: Exception thrown in \"Processing.getArtistTags\"");
         }
         return Collections.emptyList();
     }
 
-    public List<String> getSpeciesTags (String json){
+    public List<String> getSpeciesTags (String json) throws IOException {
 
         try {
             ObjectMapper objectMapper = new ObjectMapper();
@@ -152,11 +153,9 @@ public class Processing {
                     .path("tags")
                     .path("species");
 
-            List<String> generalTags = objectMapper.convertValue(generalTagsNode, List.class);
-
-            return generalTags;
+            return objectMapper.convertValue(generalTagsNode, List.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            Logging.printToLog("Warning: Exception thrown in \"Processing.getSpeciesTags\"");
         }
         return Collections.emptyList();
     }
